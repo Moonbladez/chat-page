8ec7b080edb16a83783bd3eb458beb821d2566ca
Bekki <moonbladez@hotmail.com>
2021-04-14 20:07:19 +0200

    inital commit

---
+++ components/Sidebar.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Sidebar = () => {
+  const Container = styled.div`
+    background-color: red;
+  `;
+
+  const Header = styled.div``;
+
+  return (
+    <Container>
+      <h1>sidebar</h1>
+    </Container>
+  );
+};
